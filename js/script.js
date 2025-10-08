71cb86e7a0c70d62a93c1fe0a7e1c61139308d30
JorgePena <jpenah@centro.edu.mx>
2025-10-08 10:57:34 -0600

    declaracion de variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var nombre = 'jorge';
+let edad = 22;
+const PI = Math.PI;
+const TAU = Math.PI;
+
+//booleanos tipos de variable true o false
+let esMayorDeEdad = edad >= 18 //variable formato camelcase
+
+
+console.log('hola ' + nombre);
